82b53403c56fe022dc951a90eb1c39d08ecf292a
prasanna kumara <saijsw@gmail.com>
2023-07-29 11:26:00 +0530

    Update index.js
    Third web smart contract test

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 
+// If used on the FRONTEND pass your 'clientId'
+const sdk = new ThirdwebSDK("mumbai", {
+  clientId: "ab651fdd7eb84a79ee19e4631b5ad25a",
+});
+// --- OR ---
+// If used on the BACKEND pass your 'secretKey'
+const sdk = new ThirdwebSDK("mumbai", {
+  secretKey: "wJIt8ZhbxkhPK22qJBx-hiDumFVnQihui0iEdGPM2QDNArWBHeCTblm7hiUtdx-hpXsbc6yyJXu9WB6yXZEWsQ",
+});
+
+const contract = await sdk.getContract("0xC3F4dF7076Dc21de4Ef5F82bC7cA29a418a88A52");
 export default function Home() {
   return (
     <div className="container">
